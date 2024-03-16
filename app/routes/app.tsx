72ea7f5ad652541d9a569cee435def45434c1d2c
codyrhoten <rhoten_cody@yahoo.com>
2024-03-16 17:33:13 -0400

    change spacing & button text

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -8,32 +8,30 @@ import "@shopify/polaris/build/esm/styles.css";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
+    await authenticate.admin(request);
 
-  return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
+    return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
 };
 
 export default function App() {
-  const { apiKey } = useLoaderData<typeof loader>();
+    const { apiKey } = useLoaderData<typeof loader>();
 
-  return (
-    <AppProvider isEmbeddedApp apiKey={apiKey}>
-      <ui-nav-menu>
-        <Link to="/app" rel="home">
-          Home
-        </Link>
-        <Link to="/app/additional">Additional page</Link>
-      </ui-nav-menu>
-      <Outlet />
-    </AppProvider>
-  );
+    return (
+        <AppProvider isEmbeddedApp apiKey={apiKey}>
+            <ui-nav-menu>
+                <Link to="/app" rel="home">Home</Link>
+                <Link to="/app/additional">Additional page</Link>
+            </ui-nav-menu>
+            <Outlet />
+        </AppProvider>
+    );
 }
 
 // Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
 export function ErrorBoundary() {
-  return boundary.error(useRouteError());
+    return boundary.error(useRouteError());
 }
 
 export const headers: HeadersFunction = (headersArgs) => {
-  return boundary.headers(headersArgs);
+    return boundary.headers(headersArgs);
 };
