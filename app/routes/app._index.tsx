9cacebdad1b00f2b0d27ed8828acd49aa4f11b5b
codyrhoten <rhoten_cody@yahoo.com>
2024-03-24 15:04:02 -0400

    spacing, change app URL & start on homepage functions

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useActionData, useNavigation, useSubmit } from "@remix-run/react";
@@ -17,79 +16,92 @@ import {
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
+    // const { sessionToken } = await authenticate.admin(request);
+
+    // return json(sessionToken);
     await authenticate.admin(request);
 
     return null;
 };
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-    const { admin } = await authenticate.admin(request);
-    const color = ["Red", "Orange", "Yellow", "Green"][
-        Math.floor(Math.random() * 4)
-    ];
-    const response = await admin.graphql(
-        `#graphql
-      mutation populateProduct($input: ProductInput!) {
-        productCreate(input: $input) {
-          product {
-            id
-            title
-            handle
-            status
-            variants(first: 10) {
-              edges {
-                node {
-                  id
-                  price
-                  barcode
-                  createdAt
-                }
-              }
-            }
-          }
-        }
-      }`,
-        {
-            variables: {
-                input: {
-                    title: `${color} Snowboard`,
-                    variants: [{ price: Math.random() * 100 }],
-                },
-            },
-        },
-    );
-    const responseJson = await response.json();
+    const { admin, session } = await authenticate.admin(request);
+    const response = await admin.rest.resources.Order.all({ session, status: "any" });
+    const jsonResponse = json(response.data);
 
-    return json({
-        product: responseJson.data?.productCreate?.product,
-    });
+    console.log("orders", jsonResponse)
+    return jsonResponse;
+
+    // const color = ["Red", "Orange", "Yellow", "Green"][
+    //     Math.floor(Math.random() * 4)
+    // ];
+
+    // const response = await admin.graphql(
+    //     `#graphql
+    //     mutation populateProduct($input: ProductInput!) {
+    //         productCreate(input: $input) {
+    //             product {
+    //                 id
+    //                 title
+    //                 handle
+    //                 status
+    //                 variants(first: 10) {
+    //                     edges {
+    //                         node {
+    //                             id
+    //                             price
+    //                             barcode
+    //                             createdAt
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }`,
+    //     {
+    //         variables: {
+    //             input: {
+    //                 title: `${color} Snowboard`,
+    //                 variants: [{ price: Math.random() * 100 }],
+    //             },
+    //         },
+    //     },
+    // );
+
+    // const responseJson = await response.json();
+
+    // return responseJson;
 };
 
 export default function Index() {
     const nav = useNavigation();
-    const actionData = useActionData<typeof action>();
-    const submit = useSubmit();
-    const isLoading =
-        ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
-    const productId = actionData?.product?.id.replace(
-        "gid://shopify/Product/",
-        "",
-    );
+    const orderData = useActionData<typeof action>();
+    // const submit = useSubmit();
 
-    useEffect(() => {
-        if (productId) {
-            shopify.toast.show("Product created");
-        }
-    }, [productId]);
+    // const isLoading =
+    //     ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
 
-    const generateProduct = () => submit({}, { replace: true, method: "POST" });
+    const isLoading = nav.state === "loading";
+
+    // const productId = actionData?.product?.id.replace(
+    //     "gid://shopify/Product/",
+    //     "",
+    // );
+
+    // useEffect(() => {
+    //     if (productId) {
+    //         shopify.toast.show("Product created");
+    //     }
+    // }, [productId]);
+
+    // const generateProduct = () => submit({}, { replace: true, method: "POST" });
 
     return (
         <Page>
             <ui-title-bar title="Remix app template">
-                <button variant="primary" onClick={generateProduct}>
+                {/* <button variant="primary" onClick={generateProduct}>
                     Generate a product
-                </button>
+                </button> */}
             </ui-title-bar>
             <BlockStack gap="500">
                 <Layout>
@@ -143,10 +155,10 @@ export default function Index() {
                                     </Text>
                                 </BlockStack>
                                 <InlineStack gap="300">
-                                    <Button loading={isLoading} onClick={generateProduct}>
-                                        Get my ad ROA report
+                                    <Button loading={isLoading}>
+                                        Get my ad ROAs
                                     </Button>
-                                    {actionData?.product && (
+                                    {/* {actionData?.product && (
                                         <Button
                                             url={`shopify:admin/products/${productId}`}
                                             target="_blank"
@@ -154,9 +166,9 @@ export default function Index() {
                                         >
                                             View product
                                         </Button>
-                                    )}
+                                    )} */}
                                 </InlineStack>
-                                {actionData?.product && (
+                                {/* {actionData?.product && (
                                     <Box
                                         padding="400"
                                         background="bg-surface-active"
@@ -169,7 +181,7 @@ export default function Index() {
                                             <code>{JSON.stringify(actionData.product, null, 2)}</code>
                                         </pre>
                                     </Box>
-                                )}
+                                )} */}
                             </BlockStack>
                         </Card>
                     </Layout.Section>
